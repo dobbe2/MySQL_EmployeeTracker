64055f2467ea98579285b9c1f7766d8874ed3eec
James Dobbe <dobbe2@gmail.com>
2020-03-02 08:57:39 -0800

    initial set up and psuedocoding done

---
+++ app.js
@@ -0,0 +1,31 @@
+var inquirer = require("inquirer");
+const cTable = require('console.table');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : '3306',
+    user     : 'root',
+    password : 'theLostWorld'
+  });
+   
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+
+
+console.table([
+    {
+      name: 'Mario',
+      age: 20,
+      color: "Red"
+    }, {
+      name: 'Luigi',
+      age: 20,
+      color: "Green"
+    }
+  ]);
