ad4c4281ccbabd15c6d4e6e0b310352e092273c3
James Dobbe <dobbe2@gmail.com>
2020-03-02 08:59:22 -0800

    whitespace

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     password : 'theLostWorld'
   });
    
-  connection.connect(function(err) {
+connection.connect(function(err) {
     if (err) {
       console.error('error connecting: ' + err.stack);
       return;
